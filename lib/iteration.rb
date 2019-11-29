5b3ba40984911eb2d247ea1099e31e8c54da6731
T Benson <t.benson@live.co.uk>
2019-11-29 16:12:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,7 +42,7 @@ def total_even_pairs(src)
 
   while array_index < src.count do
       sum_of_elements = (src[array_index][0] + src[array_index][1])
-      if ( sum_of_elements % 2 = 0)
+      if ( sum_of_elements % 2 == 0)
         sum_evens += sum_of_elements
       end
       array_index += 1
